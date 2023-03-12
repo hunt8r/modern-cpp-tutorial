8b3e7a48e5cdde57b789a801fe1c5becf5d3f364
hunt8r <hunt8r@users.noreply.github.com>
2023-03-11 23:31:09 -0800

    Update 2.09.decltype.auto.cpp

--- code/2/2.09.decltype.auto.cpp
+++ code/2/2.09.decltype.auto.cpp
@@ -8,15 +8,21 @@
 //
 
 template<int i>
+/ Define a struct that acts as a tag for an integer value
 struct Int {};
 
+// Declare a constexpr function that returns an Int<0> when given an Int<0>
 constexpr auto iter(Int<0>) -> Int<0>;
 
+// Declare a recursive constexpr function that takes an Int<i> as its argument,
+// creates an Int<i-1>, and calls itself with the newly created tag type
 template<int i>
 constexpr auto iter(Int<i>) {
     return iter(Int<i-1>{});
 }
 
 int main() {
+// Call iter with Int<10> to create a new tag type Int<0>
+// and store it in a variable a using decltype to deduce the type
     decltype(iter(Int<10>{})) a;
 }
